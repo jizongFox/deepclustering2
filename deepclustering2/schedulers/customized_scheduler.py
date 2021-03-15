58d3051fa62c3397bec844847a5612c54f3825fa
jizong <jizong.peng.1@etsmtl.net>
2021-03-15 20:25:29 +0800

    adding a linear scheduler

--- deepclustering2/schedulers/customized_scheduler.py
+++ deepclustering2/schedulers/customized_scheduler.py
@@ -51,7 +51,7 @@ class WeightScheduler(object):
         _current_bkend = matplotlib.get_backend()
         try:
             matplotlib.use("tkagg")
-        except:
+        except Exception:
             pass
         import matplotlib.pyplot as plt  # type: ignore
 
@@ -65,7 +65,7 @@ class WeightScheduler(object):
         self.epoch = _current_epoch
         try:
             matplotlib.use(_current_bkend)
-        except:
+        except Exception:
             pass
 
 
@@ -126,3 +126,24 @@ class ConstantScheduler(WeightScheduler):
             return 0.0
         else:
             return max_value
+
+
+class LinearScheduler(WeightScheduler):
+    def __init__(self, max_epoch, begin_value=0, end_value=1.0):
+        super().__init__()
+        self.max_epoch = max_epoch
+        self.begin_value = float(begin_value)
+        self.end_value = float(end_value)
+        self.epoch = 0
+
+    def step(self):
+        self.epoch += 1
+
+    @property
+    def value(self):
+        return self.get_lr(self.epoch)
+
+    def get_lr(self, cur_epoch):
+        return self.begin_value + (self.end_value - self.begin_value) * (
+            cur_epoch / self.max_epoch
+        )
